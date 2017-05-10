5075dc1a4be313a091fa9f63a0dc282368403059
Amit Zur <sendwithchibo@gmail.com>
2017-05-10 16:35:27 +0300

    update features

--- app/features_spec.js
+++ app/features_spec.js
@@ -1,12 +1,12 @@
 export default {
-  submission: false,
+  submission: true,
   voting: false,
   tagging: true,
   proposalsPageGroupedByTags: false,
   networking: true,
   recommendations: true,
   editAcceptedProposals: true,
-  publishAgenda: true,
+  publishAgenda: false,
   startRegistration: true,
   viewSlides: true
 }
