9af002aa57d7dfa43b83ee1d23e9550268d5b7aa
Amit Zur <sendwithchibo@gmail.com>
2017-05-14 02:01:29 +0300

    mongodb env variable

--- server/db/mongo/constants.js
+++ server/db/mongo/constants.js
@@ -1,4 +1,4 @@
-export const db = process.env.MONGO_URL || 'mongodb://localhost/ReversimSummit';
+export const db = process.env.MONGODB_URI || 'mongodb://localhost/ReversimSummit';
 
 //mongodb://reversim:123456@waffle.modulusmongo.net:27017/zy2moPyj
 
