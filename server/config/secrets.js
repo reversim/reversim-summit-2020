57023c6b909b90d852b02f59c3652c5c4493a8e9
Amit Zur <sendwithchibo@gmail.com>
2017-06-07 13:00:59 +0300

    local tokens

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -1,10 +1,17 @@
 /** Important **/
 /** You should not be committing this file to GitHub **/
 /** Repeat: DO! NOT! COMMIT! THIS! FILE! TO! YOUR! REPO! **/
+let pass = {};
+try {
+  pass = require('../../.private/pass');
+} catch(ex) {
+  console.log("Couldn't find local pass file. Relying on env");
+}
+
 export const sessionSecret = process.env.SESSION_SECRET || 'Your Session Secret goes here';
 export const google = {
-  clientID: process.env.GOOGLE_CLIENTID,
-  clientSecret: process.env.GOOGLE_SECRET,
+  clientID: pass.GOOGLE_CLIENTID || process.env.GOOGLE_CLIENTID,
+  clientSecret: pass.GOOGLE_SECRET || process.env.GOOGLE_SECRET,
   callbackURL: process.env.GOOGLE_CALLBACK || '/auth/google/callback'
 };
 export const twitter = {
