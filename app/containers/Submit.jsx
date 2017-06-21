4974ebb7c5d7a2aa5f28e9427f0bef40ca84d93d
Amit Zur <sendwithchibo@gmail.com>
2017-06-21 08:36:07 +0300

    example texts for submission

--- app/containers/Submit.jsx
+++ app/containers/Submit.jsx
@@ -248,27 +248,43 @@ class Submit extends Component {
             <h6>Public information</h6>
             <small>The following information will be presented in the website</small>
             <FormField id="fullname" label="Full name" required={true} placeholder="Your name" value={user.name} />
-            <FormField id="oneLiner" label="One Liner" value={user.oneLiner} maxLength={100} subtitle="Maximum 100 characters"/>
+            <FormField id="oneLiner" label="One Liner" value={user.oneLiner} maxLength={100} subtitle={<span>Maximum 100 characters<br/><b>Example</b>: COBOL developer at Acme Corp</span>}/>
             <FormField id="linkedin" label="Linkedin Profile" value={user.linkedin} inputType="url"/>
             <FormField id="twitter" label="Twitter @name" value={user.twitter} placeholder="@Reversim"/>
-            <FormField id="bio" label="Short Bio" value={user.bio} placeholder="" required={true} multiline={true} fullRow={true}/>
-
-
+            <FormField id="bio" label="Short Bio" value={user.bio} placeholder="" required={true} multiline={true} fullRow={true} subtitle={<span>Tell everybody a little bit about yourself. Useful sentences can be:<br/><br/>
+              “A front-end developer for the last X years”<br/>
+              “I work remotely, and interested in building remote teams and effective internal communication”<br/>
+              “I enjoy developing and using open source code”<br/>
+              “I participate in the Meetup X”<br/>
+              “I have been influenced by the book ‘The Pragmatic Programmer’”<br/>
+              “I have been playing with Deep Learning recently, took some online courses and eager to learn more!”<br/>
+              “I am an avid wikipedia contributor”</span>}/>
             <h6>Private information</h6>
             <small>The following information will be available <b>only to the organizing committee</b></small>
             <FormField id="email" label="Email" text={user.email} required={true}/>
             <FormField id="phone" label="Phone number" required={true} placeholder="05x-xxxxxxx" value={user.phone}/>
-            <FormField id="trackRecord" label="Track record as speaker" value={user.trackRecord} placeholder="" required={true} multiline={true} fullRow={true} subtitle="Your speaker track record will vastly improve your chances of getting accepted. The track record should include links to your presentations, most preferable videos of them (plus slides)"/>
-
+            <FormField id="trackRecord" label="Track record as speaker" value={user.trackRecord} placeholder="" required={true} multiline={true} fullRow={true} subtitle={<span>Your speaker track record will vastly improve your chances of getting accepted. The track record should include links to your presentations, most preferable videos of them (plus slides)<br/><br/><b>Example:</b>
+              <ul><li>&bull; ExampleCon 2017, Sweden (Keynote speaker): “Modern Fortran development with ActiveX” (45 minutes). Video: , <a target="_blank" href="https://www.youtube.com/watch?v=Nf_Y4MbUCLY">https://www.youtube.com/watch?v=Nf_Y4MbUCLY</a> slides: http://example.com/slide1</li>
+                <li>&bull; EsoteriCon 2016, Tel Aviv: “How I sold my Piet program to MOMA for $20M” (20 minutes), Video: <a target="_blank" href="https://youtu.be/DGXx56WqqJw">https://youtu.be/DGXx56WqqJw</a>, slides: http://example.com/slide2</li>
+                <li>&bull; Israeli LOLCODE meetup (February 2015), Tel Aviv, “Is LOLCODE Turing complete?” (5 minutes),  Video: <a target="_blank" href="https://www.youtube.com/watch?v=Wpx6XnankZ8">https://www.youtube.com/watch?v=Wpx6XnankZ8</a>, slides: http://example.com/slide3</li></ul></span>}/>
             <h5>Session proposal</h5>
             <small style={{marginBottom:24}}>Tell us about your talk</small>
 
             <h6>Public information</h6>
             <small>The following information will be presented in the website</small>
-            <FormField id="title" label="Title" required={true} placeholder="Title of your talk" maxLength="100"/>
+            <FormField id="title" label="Title" required={true} placeholder="Title of your talk" maxLength="100" subtitle={<span>Make it descriptive, concise, and appealing. You are welcome to review <a href="http://summit2016.reversim.com/#schedule" target="_blank">last year’s agenda</a>, or use the following examples:<br/><br/>
+            <ul><li><i>“How we optimized micro-service utilization using machine learning”</i></li>
+<li><i>“Writing on sand? Embracing CI-CD techniques in the HR team”</i></li>
+<li><i>“Effective Hackathon: How to re-write a project in 24 hours and save your startup”</i></li>
+              <li><i>“Cost of choosing the wrong development stack: A learn-build-measure story from the trenches”</i></li></ul>
+
+Reversim Summit is about deep-tech, and we will reject trivial introductory talks in software-related sessions (introduction to other topics is OK).
+            </span>}/>
             <FormField id="proposalType" inputType="radio" required={true} onChange={this.handleProposalTypeChange.bind(this)} values={proposalTypes} value={proposalType}/>
             <FormField id="video_url" label="Link to video" required={true} placeholder="e.g. http://youtu.be/xxxx" subtitle={<span><b>Seasoned speakers</b>: A link to a video of a session given in a previous conference.<br/><b>New speakers</b>: A short video introducing them and the planned session outline. Example can be found <a href="https://www.youtube.com/watch?v=2A6cLeXLLII">in this video</a>.</span>} caption={null}/>
-            <FormField id="abstract" label="Abstract" required={true} multiline={true} placeholder={`Between ${ABSTRACT_MIN}-${ABSTRACT_MAX} characters (the length of 2-5 tweets)`} subtitle={<span>Markdown syntax is supported. You can edit your proposal at any given time during the CFP period.<br/><span className={cx({'abstract-err': abstractErr})}>{abstractLen}/{ABSTRACT_MAX}</span></span>} fullRow={true} caption={null} onChange={this.onChangeAbstract}/>
+            <FormField id="abstract" label="Abstract" required={true} multiline={true} placeholder={`Between ${ABSTRACT_MIN}-${ABSTRACT_MAX} characters (the length of 2-5 tweets)`} subtitle={<span>Markdown syntax is supported. You can edit your proposal at any given time during the CFP period.<br/><br/><b>Example:</b><br/>Building an effective micro-service architecture is a non-trivial task. At example.com, we have accumulated more than 500 different micro-services over the years, ended up with a micro-service spaghetti, long latency, and inevitably -  a broken CI/CD pipeline.
+Then, we decided to remove human factor out of the equation.
+In this session I will present our open-sourced package that analyzed our microservice architecture as a graph, measured the load on each server, improved server utilization by 73% and brought our CI-CD pipeline back from the dead.<br/><br/><span className={cx({'abstract-err': abstractErr})}>{abstractLen}/{ABSTRACT_MAX}</span></span>} fullRow={true} caption={null} onChange={this.onChangeAbstract}/>
             <Tags
                   tags={tags}
                   predefinedSuggestions={predefinedTags}
@@ -291,7 +307,16 @@ class Submit extends Component {
 
             <h6>Private information</h6>
             <small>The following information will be available <b>only to the organizing committee</b></small>
-            <FormField id="outline" label="Outline" required={true} multiline={true} placeholder="" subtitle="Describe the outline of your session. An outline is a single bullet per planned slide with a short sentence of what the slide is about. (it’s OK to later change it, but please lay out your current plan). This is crucial information for being able to accept your session so please provide quality outline. Sessions with low quality outline will unfortunately not be considered." fullRow={true} caption={null}/>
+            <FormField id="outline" label="Outline" required={true} multiline={true} placeholder="" subtitle={<span>The outline should include the main subjects you intend to cover with a timing estimation and total timing. A general overview is fine, we don’t expect a per-slide description for now. <br/><br/><b>For example:</b><br/>
+
+&bull; 2m Introduction: Who am I and my professional background<br/>
+&bull; 5m Architectural overview: how we built 500 different micro-services over 5 years, and why we ended up supporting 15 different programming languages.<br/>
+&bull; 5m The latency math behind a micro-service call-chain, and why we had to over-provision containers to avoid a 1s response time because accumulated latency is not a normal distribution<br/>
+&bull; 15m Our solution: Measuring everything and using a managed machine-learning platform to optimize our response time and server utilization<br/>
+&bull; 5m: The open source power! How can you use our code to optimize your production system<br/>
+&bull; 5m Q&A<br/>
+
+Total time: 37m</span>} fullRow={true} caption={null}/>
 
             <fieldset className={cx("row")} style={{marginTop: '30px'}}>
               <div className={cx("text-center")}>
