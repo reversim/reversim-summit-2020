7971cfe1541772d73943c92aceb1f2e0706ce479
Amit Zur <sendwithchibo@gmail.com>
2017-06-21 14:20:46 +0300

    private data

--- server/db/mongo/controllers/helpers.js
+++ server/db/mongo/controllers/helpers.js
@@ -7,17 +7,17 @@ function isReversimTeamMember(loggedInUser) {
   return loggedInUser && loggedInUser.isReversimTeamMember;
 }
 
-export function transformUser(user, loggedInUser, overrideDetails) {
+export function transformUser(user, loggedInUser) {
   if (user._doc) {
     user = user._doc;
   }
 
-  let isReversimMember = overrideDetails || isReversimTeamMember(loggedInUser);
+  let isReversimMember = isReversimTeamMember(loggedInUser);
 
   if (_.isObject(user) && _.has(user, 'profile')) {
     return {
       _id: user._id,
-      proposals: user.proposals && user.proposals.map(p => transformProposal(p, loggedInUser, overrideDetails)),
+      proposals: user.proposals && user.proposals.map(p => transformProposal(p, loggedInUser)),
       name: user.profile && user.profile.name,
       oneLiner: user.profile && user.profile.oneLiner,
       email: isReversimMember && user.email,
@@ -29,17 +29,17 @@ export function transformUser(user, loggedInUser, overrideDetails) {
       linkedin: user.profile && user.profile.linkedin,
       twitter: user.profile && user.profile.twitter,
       stackOverflow: user.profile && user.profile.stackOverflow,
-      phone: user.profile && user.profile.phone
+      phone: isReversimMember && user.profile && user.profile.phone
     };
   }
 
   return user;
 }
 
-export function transformProposal(proposal, loggedInUser, overrideDetails) {
+export function transformProposal(proposal, loggedInUser) {
 
   if (_.isObject(proposal)) {
-    let isReversimMember = overrideDetails || isReversimTeamMember(loggedInUser);
+    let isReversimMember = isReversimTeamMember(loggedInUser);
 
     return {
       id: proposal.id,
@@ -47,16 +47,16 @@ export function transformProposal(proposal, loggedInUser, overrideDetails) {
       abstract: proposal.abstract,
       type: proposal.type,
       tags: proposal.tags,
-      status: proposal.status,
+      status: isReversimMember && proposal.status,
       speaker_ids: proposal.speaker_ids && proposal.speaker_ids.map((user) => {
-        return transformUser(user, loggedInUser, overrideDetails);
+        return transformUser(user, loggedInUser);
       }),
       startTime: proposal.startTime,
       endTime: proposal.endTime,
       hall: proposal.hall,
       slides_gdrive_id: proposal.slides_gdrive_id,
-      video_url: proposal.video_url,
-      outline: proposal.outline,
+      video_url: isReversimMember && proposal.video_url,
+      outline: isReversimMember && proposal.outline,
       total: (proposal.attendees && isReversimMember) ? proposal.attendees.length : undefined,
       attended: proposal.attendees && (loggedInUser ? proposal.attendees.indexOf(loggedInUser._id) > -1 : false)
     }
