60875682937152815c2fcf89ccced2cc9a3066ea
Amit Zur <sendwithchibo@gmail.com>
2017-07-20 21:33:16 +0300

    datorama logo

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -41,7 +41,7 @@ var commonLoaders = [
     loader: ExtractTextPlugin.extract('style-loader', 'css-loader?module!postcss-loader')
   },
   { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
-  { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+  { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader", exclude: /datorama/ }
 ];
 
 var postCSSConfig = function() {
