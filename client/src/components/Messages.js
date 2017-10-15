3603e1a4420d68188dfbf577da35a2c603f3b0f2
Amit Zur <sendwithchibo@gmail.com>
2017-10-15 11:01:44 +0300

    messages support markdown

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import cn from 'classnames';
 import s from './Messages.css';
+import ReactMarkdown from 'react-markdown';
 
 const Messages = ({ messages }) => {
 	if (messages.length) {
 		return <ul className={cn(s.messages, "font-size-lg")}>
 				{messages.map(msg => (
-					<li key={msg._id} className={s.message}><i className="fa fa-info-circle mr-3"/>{msg.text}</li>
+					<li key={msg._id} className={s.message}><i className="fa fa-info-circle mr-3"/>
+						<ReactMarkdown source={msg.text} />
+					</li>
 				))}
 			</ul>
 	} else {
