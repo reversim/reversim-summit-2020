99633591bd1881a01ae322f85538dab5f6fa2046
Ran Tavory <rantav@gmail.com>
2018-01-01 23:27:28 +0200

    Export and Aggregate from the DB

---
+++ csv_aggregate.js
@@ -0,0 +1,35 @@
+p = db.proposals;
+p.aggregate(
+  {$project: {_id: 0, id: 1, title: 1, type: 1, tags: 1, speaker_ids: 1 }},
+  {$unwind: "$speaker_ids"},
+  {$lookup: {
+    localField: "speaker_ids",
+    from: "users",
+    foreignField: "_id",
+    as: "speaker"
+  }},
+  {$unwind: "$speaker"},
+  {$project: {
+    _id: 0,
+    link: { $concat: ["https://summit2017.reversim.com/session/", "$id"]},
+    title: 1,
+    type: 1,
+    tags: {
+      $reduce: {
+        input: "$tags",
+        initialValue: "",
+        in: {
+          $cond: {
+            "if": { $eq: [{ $indexOfArray: ["$tags", "$$this"] }, 0]},
+            "then": { $concat: ["$$value", "$$this"] },
+            "else": { $concat: ["$$value", ",", "$$this"] }
+          }
+        }
+      }
+    },
+    speaker: "$speaker.profile.name",
+    email: "$speaker.email"
+  }
+  }
+  ,{$out: "proposal_results"}
+);
