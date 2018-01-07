6b11b710956980d34c43a531d77d506d34a9589a
Ran Tavory <rantav@gmail.com>
2018-01-07 14:17:25 +0200

    Do not require app.use(require('force-ssl-heroku'))

--- server/init/express.js
+++ server/init/express.js
@@ -25,7 +25,9 @@ export default (app) => {
 
   app.set('view cache', false);
 
-  app.use(require('force-ssl-heroku'));
+
+  // As a temporary means, we're not forcing SSL on heroku in order to allow migration to GH pages and let HSTS cool down.
+  // app.use(require('force-ssl-heroku'));
 
   app.use(bodyParser.json({limit: '50mb'}));
   app.use(bodyParser.urlencoded({limit: '50mb', extended: true})); // for parsing application/x-www-form-urlencoded
