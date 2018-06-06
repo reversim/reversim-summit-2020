6138f57756893249e3bbbe60f8791f3b8baa3649
Alon Nativ <alon.nativ@gmail.com>
2018-06-06 22:02:29 +0300

    typo fix

--- server/init/eventConfig.js
+++ server/init/eventConfig.js
@@ -1,18 +1,18 @@
 import moment from 'moment-timezone';
 
 const config = {
-  cfpStateDate: process.env.CFP_START_DATE ||  "2018-04-25",
+  cfpStartDate: process.env.CFP_START_DATE ||  "2018-04-25",
   cfpEndDate: process.env.CFP_END_DATE || "2018-05-31",
   votingStartDate: process.env.VOTING_START_DATE || "2018-06-01",
   votingEndDate: process.env.VOTING_END_DATE || "2018-06-10",
 }
 
 export default () => {
-  const cfp = moment().tz('Israel').isBetween(config.cfpStateDate, config.cfpEndDate, 'day', "[]");
+  const cfp = moment().tz('Israel').isBetween(config.cfpStartDate, config.cfpEndDate, 'day', "[]");
   const voting = moment().tz('Israel').isBetween(config.votingStartDate, config.votingEndDate, 'day', "[]");
   
   return {
-    cfpStateDate: config.cfpStateDate,
+    cfpStartDate: config.cfpStartDate,
     cfpEndDate: config.cfpEndDate,
     votingStartDate: config.votingStartDate,
     votingEndDate: config.votingEndDate,
