948d6c9e34e6dad40b0189e72f6651950e635452
Amit Zur <sendwithchibo@gmail.com>
2018-08-02 18:40:55 +0300

    small fixes

--- client/src/components/Location.js
+++ client/src/components/Location.js
@@ -8,7 +8,7 @@ const Location = () => (
       <h1 className="mb-0" style={{zIndex: 1, position: 'relative'}}>
         Venue
       </h1>
-      <div className="bg-emph py-9 px-4 d-inline-block" style={{marginTop: -40}}>
+      <div className="bg-emph py-9 px-4 d-inline-block h1-up">
         <h3 className="mb-0">Tel Aviv University, Smolarz Auditorium</h3>
       </div>
       <div style={{marginTop: -20}} className="ml-4">
@@ -16,7 +16,7 @@ const Location = () => (
       </div>
       <div className="d-flex align-items-center mt-9">
         <div className="border border-cyan mr-4 flex-1" />
-        <a href="/location">
+        <a href="http://smolarz.tau.org.il/access.html" target="_blank" rel="noopener noreferrer">
           <Button>More info from TAU</Button>
         </a>
       </div>
