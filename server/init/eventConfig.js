9ff3725e25022caf74b2a9796d0b379c975cca0d
neta <neta.ked@gmail.com>
2019-01-20 01:18:43 +0200

    set dates to the proposals

--- server/init/eventConfig.js
+++ server/init/eventConfig.js
@@ -1,8 +1,7 @@
 import moment from 'moment-timezone';
 
 const config = {
-  // TODO neta- set true date
-  cfpStartDate: process.env.CFP_START_DATE ||  "2019-1-20",
+  cfpStartDate: process.env.CFP_START_DATE ||  "2019-01-20",
   cfpEndDate: process.env.CFP_END_DATE || "2019-02-20",
   votingStartDate: process.env.VOTING_START_DATE || "2019-04-01",
   votingEndDate: process.env.VOTING_END_DATE || "2019-05-01",
