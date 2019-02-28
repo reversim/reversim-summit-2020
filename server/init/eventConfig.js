1b502c895ea5bf5383651ce9d9ca0bce53264aa3
neta_kedem <neta.pepper@gmail.com>
2019-02-28 20:02:29 +0200

    set cfp to end at 8 am

--- server/init/eventConfig.js
+++ server/init/eventConfig.js
@@ -3,12 +3,12 @@ import moment from 'moment-timezone';
 const config = {
   cfpStartDate: process.env.CFP_START_DATE ||  "2019-01-20",
   cfpEndDate: process.env.CFP_END_DATE || "2019-02-28",
-  votingStartDate: process.env.VOTING_START_DATE || "2019-04-01",
-  votingEndDate: process.env.VOTING_END_DATE || "2019-05-01",
+  votingStartDate: process.env.VOTING_START_DATE || "2019-03-07",
+  votingEndDate: process.env.VOTING_END_DATE || "2019-04-01",
 }
 
 export default () => {
-  const cfp = moment().tz('Israel').isBetween(config.cfpStartDate, config.cfpEndDate, 'day', "[]");
+  const cfp = moment().tz('Israel').subtract(8, 'hours').isBetween(config.cfpStartDate, config.cfpEndDate, 'day', "[]");
   const voting = moment().tz('Israel').isBetween(config.votingStartDate, config.votingEndDate, 'day', "[]");
   const votingEnded = moment().tz('Israel').isAfter(config.votingEndDate, 'day', "[]");
 
