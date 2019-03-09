e3b7cfbb4dc375571cf56a0fc58934a32af7354c
Ran Tavory <rantav@gmail.com>
2019-03-09 20:40:34 +0200

    Add stg import capability

--- scripts/stg_mongo_backup.sh
+++ scripts/stg_mongo_backup.sh
@@ -7,7 +7,7 @@ for c in proposals users sponsors
 do
     file=".private/backup-$d/$c.json"
     echo "Exporting $c"
-    mongoexport -h $MONGODB_HOST -d $MONGODB_DB -c $c -u $MONGODB_READONLY_USER -p $MONGODB_READONLY_PASS -o $file
+    mongoexport -h $STG_MONGODB_HOST -d $STG_MONGODB_DB -c $c -u $STG_MONGODB_READONLY_USER -p $STG_MONGODB_READONLY_PASS -o $file
 
     if [ "$1" = "--import" ]; then
         echo "Importing $c"
