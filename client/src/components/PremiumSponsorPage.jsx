9d50f97419774db1c2616c8869d203eb016b4d9b
Ran Tavory <rantav@gmail.com>
2019-03-15 14:06:04 +0200

    Fix premium sponsor internal navigagion links

--- client/src/components/PremiumSponsorPage.jsx
+++ client/src/components/PremiumSponsorPage.jsx
@@ -100,14 +100,14 @@ const SponsorPage = ({ sponsor, color, isFull, ...props }) => {
                         </li>
                         {!!sponsor.openPositions.length && (
                             <li>
-                                <ScrollLink href="#" to={"Open Positions"} offset={-100}>
+                                <ScrollLink href="#" to={"open-positions"} offset={-100}>
                                     Open Positions
                                 </ScrollLink>
                             </li>
                         )}
                         {sponsor.reversimAndUs && (
                             <li>
-                                <ScrollLink href="#" to={"Reversim & Us"} offset={-100}>
+                                <ScrollLink href="#" to={"reversim-and-us"} offset={-100}>
                                     Reversim & Us
                                 </ScrollLink>
                             </li>
@@ -117,7 +117,7 @@ const SponsorPage = ({ sponsor, color, isFull, ...props }) => {
             </Container>
             <Container>
                 <div className="premium-pr premium-mr">
-                    <section className="premium-who">
+                    <section className="premium-who" name="about">
                         <div className="d-flex">
                             <h3 className="font-size-xl text-purple2">Who We Are?</h3>
                             <div className="hl bg-purple2" />
@@ -158,7 +158,7 @@ const SponsorPage = ({ sponsor, color, isFull, ...props }) => {
                 {
                     (sponsor.openPositions && sponsor.openPositions.length > 0) && (
                         <div className="premium-mr">
-                            <section>
+                            <section name="open-positions">
                                 <div className="d-flex align-items-center mb-8 mt-12">
                                     <img src={zigzag} alt="" height="80" style={{marginRight: -11}}/>
                                     <h3 className="text-purple2 font-size-xl mr-4">
@@ -182,10 +182,10 @@ const SponsorPage = ({ sponsor, color, isFull, ...props }) => {
                 {
                     sponsor.reversimAndUs && (
                         <div className="premium-mr">
-                            <section>
+                            <section name="reversim-and-us">
                                 <div className="d-flex align-items-center mb-4 mt-12">
                                     <h3 className="text-purple2 font-size-xl mr-4">
-                                        Reversim and us
+                                        Reversim & Us
                                     </h3>
                                     <div className="flex-grow-1 border-bottom border-purple2"/>
                                 </div>
@@ -293,94 +293,6 @@ class SponsorCarousel extends React.Component {
         className={cn(s.carouselControl, "cursor-pointer bg-purple2")}
         />
         </Carousel>
-    );
-    }
-}
-
-class DescriptionSection extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            // isEditing: false,
-            isLoading: false
-        };
-    }
-
-    render() {
-        // const {isEditingiting} = this.state;
-        const { sponsor, canEdit } = this.props;
-        console.log("sponsor", sponsor);
-        return (
-            <Container>
-            <div className="pt-8">
-            <div className={cn("d-flex", s.premiumSection)} name="about">
-            <div className="width-half mr-4">
-            <div className="d-flex">
-            <div className="font-size-xl text-purple2">Who We Are?</div>
-        <div className="hl bg-purple2" />
-            </div>
-            <div className="text-black-50">{sponsor.about}</div>
-        </div>
-        <div className="width-half ml-4">
-            <SponsorCarousel sponsor={sponsor} />
-        </div>
-        </div>
-        </div>
-        <div className="pt-8">
-            <div className={cn("d-flex", s.premiumSection)} name="Tech-Story">
-            <div className="font-size-xl text-purple2">
-            Our Technology Story
-        </div>
-        <div className="hl bg-purple2" />
-            </div>
-            <div className="d-flex">
-            <div className="text-black-50 width-half">
-            {sponsor.techStory.text}
-    </div>
-        <div className="d-flex align-content-start flex-wrap">
-            {sponsor.techStory.technologies.map(
-                (technology, i) =>
-                    technology && (
-                    <div
-        className="bg-purple2 text-white w-max-content h-max-content p-1 m-2"
-        key={i}
-            >
-            {technology}
-            </div>
-    )
-    )}
-    </div>
-        </div>
-        </div>
-        {!!sponsor.openPositions.length && (
-        <div className="pt-8" name="Open Positions">
-            <div className={cn("d-flex", s.premiumSection)}>
-        <div className="font-size-xl text-purple2">
-            Open Positions - Join Us
-        </div>
-        <div className="hl bg-purple2" />
-            </div>
-            <div className="text-black-50 d-flex">
-            {sponsor.openPositions.map((openPosition, i) => (
-                <OpenPosition
-            key={i}
-            openPosition={openPosition}
-            {...this.props}
-            />
-        ))}
-        </div>
-        </div>
-        )}
-        {sponsor.reversimAndUs && (
-        <div className="pt-8" name="Reversim & Us">
-            <div className={cn("d-flex", s.premiumSection)}>
-        <div className="font-size-xl text-purple2">Reversim and us</div>
-        <div className="hl bg-purple2" />
-            </div>
-            <div className="text-black-50">{sponsor.reversimAndUs}</div>
-        </div>
-        )}
-    </Container>
     );
     }
 }
