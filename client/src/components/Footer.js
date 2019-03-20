cd816dc4cf82f1746cec0254e62040e233dff440
neta_kedem <neta.pepper@gmail.com>
2019-03-20 07:33:14 +0200

    better footer for mobile

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,52 +3,56 @@ import { Container, Row } from "reactstrap";
 import { getLoginUrl } from "./Redirect";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons";
-
+import cn from 'classnames';
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 library.add(faFacebook, faTwitter, faEnvelope);
 
-const Footer = () => (
+const Footer = ({isSmallScreen}) => (
   <footer className="bg-purple3 py-6 font-size-md mt-13">
     <Container>
+      <div className={isSmallScreen && "d-flex justify-content-center"}>
       <Row
-        className="justify-content-center justify-content-lg-between align-items-center"
+        className={cn("justify-content-center justify-content-lg-between text-white", isSmallScreen ? "d-flex flex-column w-max-content" : "align-items-center")}
         noGutters
       >
-        <div className="d-flex">
-          <div className="mr-10 text-white d-flex">
-            <div className="mr-2">Contact us:</div>
-            <a href="mailto:rs19team@googlegroups.com">
-              <FontAwesomeIcon
-                className="mr-2"
-                color={"white"}
-                icon={faEnvelope}
-              />
-            </a>
-            <div className="mr-2 ml-5">Stay in touch:</div>
-            <a href="https://www.facebook.com/groups/reversim/">
-              <FontAwesomeIcon
-                className="mr-2"
-                color={"white"}
-                icon={faFacebook}
-              />
-            </a>
-            <a href="https://twitter.com/reversim/">
-              <FontAwesomeIcon
-                className="mr-2"
-                color={"white"}
-                icon={faTwitter}
-              />
-            </a>
-          </div>
-          <a className="text-white" href="http://confcodeofconduct.com/">
+        <div className={cn("d-flex", isSmallScreen && 'flex-column')}>
+            <div className="d-flex">
+              <div>Contact us:</div>
+              <a href="mailto:rs19team@googlegroups.com">
+                <FontAwesomeIcon
+                  className="ml-2"
+                  color={"white"}
+                  icon={faEnvelope}
+                />
+              </a>
+            </div>
+            <div className={cn("d-flex", !isSmallScreen && "ml-7")}>
+              <div>Stay in touch:</div>
+              <a href="https://www.facebook.com/groups/reversim/">
+                <FontAwesomeIcon
+                  className="ml-2"
+                  color={"white"}
+                  icon={faFacebook}
+                />
+              </a>
+              <a href="https://twitter.com/reversim/">
+                <FontAwesomeIcon
+                  className="ml-2"
+                  color={"white"}
+                  icon={faTwitter}
+                />
+              </a>
+            </div>
+          <a className={cn("text-white", !isSmallScreen && "ml-12")} href="http://confcodeofconduct.com/">
             Code of Conduct
           </a>
         </div>
-        <span className="d-flex align-items-center text-white">
+        <div className="d-flex align-items-center text-white">
           All Rights Reserved © 2019
-        </span>
+        </div>
       </Row>
+      </div>
     </Container>
   </footer>
 );
