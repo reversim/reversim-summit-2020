3d03414cdd79d2b15896fe8625ab3599aba849b1
Ran Tavory <rantav@gmail.com>
2019-03-24 21:55:28 +0200

    Simplify the votes exporter script

--- scripts/export_votes.js
+++ scripts/export_votes.js
@@ -1,13 +1,9 @@
-db.proposals_votes.remove({});
-db.proposals.aggregate(
-  {$project: {
-    _id: "$_id",
-    votes: {$size: "$attendees"}
+db.proposals.aggregate({
+    $project: {
+      _id: "$_id",
+      votes: {$size: "$attendees"}
+    }
   }
-  }
-  ,{$out: "proposals_votes"}
-);
-
-db.proposals_votes.find().forEach(function(p){
+).forEach(function(p){
   print(p._id.str, ", ", p.votes);
 });
