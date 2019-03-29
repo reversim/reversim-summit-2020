7ee8074bbae04acd6aa721b54bd895f0869e7fa6
Ran Tavory <rantav@gmail.com>
2019-03-29 14:59:31 +0300

    Fix scripts/export_votes.js

--- scripts/export_votes.js
+++ scripts/export_votes.js
@@ -1,9 +1,18 @@
-db.proposals.aggregate({
+db.proposals.aggregate([
+  {
+    $match: {
+      status: {
+        $nin: ['archived', 'deleted']
+      }
+    }
+  },
+  {
     $project: {
       _id: "$_id",
       votes: {$size: "$attendees"}
     }
   }
+]
 ).forEach(function(p){
   print(p._id.str, ", ", p.votes);
 });
