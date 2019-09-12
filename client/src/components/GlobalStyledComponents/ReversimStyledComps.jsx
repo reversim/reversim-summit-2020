3100192f000666836eb3ac0f36db03a5d59bc107
yarivShamash <yariv.shamash@gmail.com>
2019-09-12 18:55:10 +0300

    Themed the Container in ReversimStyledComps.js

--- client/src/components/GlobalStyledComponents/ReversimStyledComps.jsx
+++ client/src/components/GlobalStyledComponents/ReversimStyledComps.jsx
@@ -1,28 +1,40 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: space-evenly;
+  ${props => {
+    const {
+      s,
+      m,
+      l,
+      xl,
+    } = props.theme.mq;
 
-  width: 100%;
-  padding-right: 1rem;
-  padding-left: 1rem;
-  margin-right: auto;
-  margin-left: auto;
-  @media (min-width: 576px) {
-    max-width: 540px;
-  };
-  @media (min-width: 768px) {
-    max-width: 720px;
-  };
-  @media (min-width: 992px) {
-    max-width: 960px;
-  };
-  @media (min-width: 1200px) {
-    max-width: 1280px;
-  };
+    return (`
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+      justify-content: space-evenly;
+
+      width: 100%;
+      padding-right: 1rem;
+      padding-left: 1rem;
+      margin-right: auto;
+      margin-left: auto;
+      @media (min-width: ${s}) {
+        max-width: 440px;
+      };
+      @media (min-width: ${m}) {
+        max-width: 720px;
+      };
+      @media (min-width: ${l}) {
+        max-width: 960px;
+      };
+      @media (min-width: ${xl}) {
+        max-width: 1280px;
+      };    
+    `)
+  }}
+
 `
 
 export const ButtonStyledLink = styled.a`
