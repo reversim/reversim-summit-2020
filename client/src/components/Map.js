ba0789c7d3d8029440ae78a6c7b9714574cab03f
yarivShamash <yariv.shamash@gmail.com>
2019-10-24 20:41:00 +0300

    Incorporated s-c into Map.js

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,14 +1,37 @@
-import React from "react";
-import { withGoogleMap, GoogleMap, Marker, withScriptjs } from "react-google-maps";
+import React from 'react';
+import styled from 'styled-components';
+
+import { withGoogleMap, GoogleMap, Marker, withScriptjs } from 'react-google-maps';
 // import withScriptjs from "react-google-maps/lib/async/withScriptjs";
-import s from "./Map.css";
-import cn from "classnames";
 
 const position = {
   lat: 32.104836,
   lng: 34.807388
 };
 
+// React components section
+const MapContainer = styled.div`
+  ${ ({ theme: { space, mq, color } }) => `
+    height: 480px;
+    width: 480px;
+    margin-right: ${space.xxl};
+
+    border: 4px solid ${color.box_shadow_1};
+
+    @media (max-width: ${mq.l}) {
+      width: 100%;
+      margin-right: 0;
+    }
+  `}
+`;
+
+const MapElement = styled.div`
+  position: relative;
+  overflow: hidden;
+  height: 100%;
+`;
+
+// React components section
 const MapWithLoader = withScriptjs(
   withGoogleMap(() => {
     const markerEl = <Marker position={position} defaultAnimation={2} />;
@@ -26,11 +49,9 @@ const Map = () => (
     loadingElement={<div />}
     googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&key=${
       process.env.REACT_APP_GOOGLE_MAPS_KEY
-      }`}
-    containerElement={
-      <div className={cn(s.mapContainer, "b-strong border-purple2")} />
-    }
-    mapElement={<div className={s.mapEl} />}
+    }`}
+    containerElement={<MapContainer />}
+    mapElement={<MapElement />}
   />
 );
 
