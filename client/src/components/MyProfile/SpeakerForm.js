08d1040339061d7472edfde7891f78bdc820e3e7
yarivShamash <yariv.shamash@gmail.com>
2020-03-16 15:42:25 +0200

    deleted the redundent <input>

--- client/src/components/MyProfile/SpeakerForm.js
+++ client/src/components/MyProfile/SpeakerForm.js
@@ -1,12 +1,18 @@
 //SpeakerForm.js //DELETE WHEN DONE
 
 import React from 'react';
-import {Button, Input} from 'reactstrap';
-import UserForm, {getUserData} from '../CFP/UserForm';
 import ga from 'react-ga';
-import {Link} from 'react-router-dom';
-import {getHref} from '../../utils';
 
+import {Link} from 'react-router-dom';
+import {Button, Input} from 'reactstrap';
+import {getHref} from '../../utils';
+import UserForm, {getUserData} from '../CFP/UserForm';
+
+// styled-components components
+
+
+
+// React components
 class SpeakerForm extends React.Component {
   handleSubmit = async e => {
     e.preventDefault();
@@ -33,13 +39,14 @@ class SpeakerForm extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <UserForm user={user} />
-        <Input type="submit" className="d-none" />
+
         <div className="d-flex justify-content-center align-items-center">
           <Button color="primary" className="styled-button w-max-content btn btn-secondary mr-4">
             Submit
           </Button>
           <Link to={`/speaker/${getHref(user)}`}>Cancel</Link>
         </div>
+
       </form>
     );
   }
